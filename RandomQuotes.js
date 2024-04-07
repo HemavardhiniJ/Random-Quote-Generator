c2bb9de73f7900c487473db1c8d47d0c4d443283
HemavardhiniJ <113229774+HemavardhiniJ@users.noreply.github.com>
2024-04-07 17:19:20 +0530

    Add files via upload

---
+++ RandomQuotes.js
@@ -0,0 +1,13 @@
+
+function newQuote(){
+fetch("https://type.fit/api/quotes")
+  .then((data)=>{
+    return data.json();
+  })
+  .then((data)=> {
+    let random=Math.floor(Math.random()*data.length)
+    document.getElementById('quote').innerText=data[random].text;
+    document.getElementById('author').innerText=data[random].author;
+  });
+}
+newQuote()
